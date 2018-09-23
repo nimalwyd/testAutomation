32543c6c2792465c4050512cda6777dacfd76acf
nimal <nimal@DESKTOP-AVRBRCH>
2018-09-23 02:03:13 -0400

    made changes to HomePageTest.java

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# About the project
+
+Test automation for web portals in Hybrid framework with TDD,BDD charecteristics
+
+
+### Prerequisites
+
+Eclipse
+
+Selenium WebDriver
+
+TestNG
+
+Maven
+
+Apache POI
+
+Log4J
+
+
+```
+## Project status
+completed
+```
+
+## Built With
+
+* [Windows]
+* [Java]
+* [Selenium WebDriver 3.9]
+
+```
+## Author
+
+* **Nimal krishna** - *14/12/2017*
+```
--- src/test/java/com/crm/qa/testcases/HomePageTest.java
+++ src/test/java/com/crm/qa/testcases/HomePageTest.java
@@ -1,3 +1,21 @@
+/**********************************************************************;
+	* Project           : Selenium webdriver Mobile automation front end Hybrid framework
+	*
+	* Program name      : AppiumAutomationFramework
+	*
+	* Author            : Nimal krishna
+	*
+	* Date created      : 20160816
+	*
+	* Purpose           : Simple Gui Web portal automation framework
+	*
+	* Revision History  :
+	*
+	* Date        Author      Ref    Revision (Date in YYYYMMDD format) 
+	* 20180818    Nimal      1      added BDD,TDD characters to the framework 
+	*
+	**********************************************************************/
+
 package com.crm.qa.testcases;
 
 import static org.testng.Assert.assertEquals;
@@ -27,23 +45,7 @@ public class HomePageTest extends TestBase {
 	PaymentPage paymentPage;
 	TestUtil testUtil;
 	String sheetName = "Sheet3";
-	/**********************************************************************;
-	* Project           : Selenium webdriver Mobile automation front end Hybrid framework
-	*
-	* Program name      : AppiumAutomationFramework
-	*
-	* Author            : Nimal krishna
-	*
-	* Date created      : 20160816
-	*
-	* Purpose           : Simple Gui Web portal automation framework
-	*
-	* Revision History  :
-	*
-	* Date        Author      Ref    Revision (Date in YYYYMMDD format) 
-	* 20180818    Nimal      1      added BDD,TDD characters to the framework 
-	*
-	|**********************************************************************/
+	
 	
 	
 	public HomePageTest() {
